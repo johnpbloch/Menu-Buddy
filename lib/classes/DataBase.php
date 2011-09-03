f0c83fc8132bda134e3cfec7c940e7e7c4f85f18
John P. Bloch <johnpbloch@gmail.com>
2011-09-03 19:16:04 -0400

    Allow optional vsprintf prepare statements that return a prepared SQL statement rather than a MYSQLI_STMT object. Props WordPress on this one.

--- lib/classes/DataBase.php
+++ lib/classes/DataBase.php
@@ -3,7 +3,7 @@
 namespace MenuBuddy;
 
 class DataBase extends \mysqli {
-	
+
 	protected $prefix;
 
 	public function __construct( $name='', $user='', $pass='', $host='', $prefix='' ) {
@@ -17,18 +17,36 @@ class DataBase extends \mysqli {
 			$host = MB_DB_HOST;
 		if( empty ($prefix) )
 			$prefix = MB_DB_PREFIX;
-		
+
 		$host = ('p:' == substr($host, 0, 2)) ? $host : "p:$host";
-		
+
 		$this->prefix = $prefix;
-		
+
 		parent::__construct( $host, $user, $pass, $name );
-		
+
 		if($this->connect_error){
 			die( '<h1>Database Connection Error (' . $this->connect_errno . ')</h1><p>' . $this->connect_error . '</p>' );
 		}
-		
+
 		$this->set_charset('utf8');
+	}
+
+	public function prepare($query) {
+		$args = func_get_args();
+		array_shift($args);
+		if(empty ($args))
+			return parent::prepare ($query);
+		if(!empty($args[0]) && is_array($args[0]))
+			$args = $args[0];
+		$query = str_replace('"%s"', '%s', $query);
+		$query = str_replace("'%s'", '%s', $query);
+		$query = preg_replace('@(?<!%)%s@', "'%s'", $query);
+		array_walk($args, array($this, '_real_escape_by_ref'));
+		return @vsprintf($query, $args);
+	}
+
+	public function _real_escape_by_ref(&$string){
+		$string = $this->real_escape_string($string);
 	}
 
 }

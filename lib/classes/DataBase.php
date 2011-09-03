6c454e9c0555dc6fed5f226350015e0994dde6f4
John P. Bloch <johnpbloch@gmail.com>
2011-09-03 19:57:50 -0400

    Begin table initialization procedure, table installation.

--- lib/classes/DataBase.php
+++ lib/classes/DataBase.php
@@ -41,6 +41,8 @@ class DataBase extends \mysqli {
 		}
 
 		$this->set_charset('utf8');
+		
+		$this->init_tables();
 	}
 
 	public function prepare($query) {
@@ -75,6 +77,37 @@ class DataBase extends \mysqli {
 		}
 		$results->close();
 		return $result;
+	}
+	
+	private function init_tables(){
+		$tables = array(
+			'users',
+		);
+		$check_tables = (defined('CHECK_TABLES') && CHECK_TABLES);
+		$table_list = $check_tables ? $this->fetch_table_list() : false;
+		foreach($tables as $table){
+			$this->$table = $this->prefix . $table;
+			if($check_tables && !in_array($this->$table, $table_list)){
+				$this->install_table( $table );
+			}
+		}
+	}
+	
+	private function fetch_table_list() {
+		$tables = $this->query('SHOW TABLES');
+		$table_list = array();
+		if($tables === false || $tables->num_rows == 0)
+			return $table_list;
+		while( $table = $tables->fetch_row() ){
+			$table_list = array_merge($table_list, $table);
+		}
+		return $table_list;
+	}
+	
+	private function install_table( $table ){
+		/**
+		 * @todo schema installation
+		 */
 	}
 
 }

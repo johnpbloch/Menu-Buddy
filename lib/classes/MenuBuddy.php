8acf12ea94fcc14df0f227d73be250240bcb6067
John P. Bloch <johnpbloch@gmail.com>
2011-09-03 12:11:31 -0400

    Abstract controller class and MenuBuddy class to extend it and act as the program's main controller.

---
+++ lib/classes/MenuBuddy.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MenuBuddy;
+
+class MenuBuddy extends Controller {
+	
+	function __construct() {
+		$this->parse_request();
+	}
+	
+	public function run(){
+		echo '<pre>';
+		var_dump($_SERVER);
+		echo '</pre>';
+	}
+	
+}

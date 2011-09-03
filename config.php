22b831d6f50ef0292fe12c1a5f2f04de8a4237d9
John P. Bloch <johnpbloch@gmail.com>
2011-09-03 12:07:28 -0400

    Set up a basic config file. Allows for local configuration or direct editing of the file. Sets up some necessary constants.

---
+++ config.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MenuBuddy;
+
+define( 'LIB', PATH . 'lib/' );
+define( 'CLASSES', LIB . 'classes/' );
+define( 'FUNC', LIB . 'functions/' );
+
+// To turn off error reporting the next line should be '/*'. To turn it on it should be '//*'
+//*
+error_reporting(E_ALL);
+ini_set( 'display_errors', 1 );
+// */
+
+if(file_exists( PATH . 'local-config.php' ))
+	require( PATH . 'local-config.php' );
+
+if( !defined('MB_DB_NAME') )
+	define( 'MB_DB_NAME', '' );
+
+if( !defined('MB_DB_USER') )
+	define( 'MB_DB_USER', '' );
+
+if( !defined('MB_DB_PASS') )
+	define( 'MB_DB_PASS', '' );
+
+if( !defined('MB_DB_HOST') )
+	define( 'MB_DB_HOST', 'localhost' );
+
+if( !defined('MB_DB_PREFIX') )
+	define( 'MB_DB_PREFIX', 'mbd_' );

0f63c0ba64d366cf047fd1498f7f4319570342b2
John P. Bloch <johnpbloch@gmail.com>
2011-09-03 20:31:00 -0400

    Reformat the schema to make it look pretty.

--- schema/Users.php
+++ schema/Users.php
@@ -1,18 +1,18 @@
 <?php
 
-if(!isset($this))
+if (!isset($this))
 	return;
 
 $schema = "CREATE TABLE `{$this->dbname}`.`{$this->$table}` (
-  `ID` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `Login` VARCHAR(60)  NOT NULL,
-  `Email` VARCHAR(100)  NOT NULL,
-  `DisplayName` VARCHAR(60)  NOT NULL,
-  `DateCreated` DATETIME  NOT NULL,
-  `Pass` VARCHAR(64)  NOT NULL,
-  PRIMARY KEY (`ID`),
-  INDEX `Login`(`Login`),
-  INDEX `Email`(`Email`)
+	`ID` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`Login` VARCHAR(60)  NOT NULL,
+	`Email` VARCHAR(100)  NOT NULL,
+	`DisplayName` VARCHAR(60)  NOT NULL,
+	`DateCreated` DATETIME  NOT NULL,
+	`Pass` VARCHAR(64)  NOT NULL,
+	PRIMARY KEY (`ID`),
+	INDEX `Login`(`Login`),
+	INDEX `Email`(`Email`)
 )
 ENGINE = MyISAM;";
 

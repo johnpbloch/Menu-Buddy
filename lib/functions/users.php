f9b7f1fe726f54143d212f15b86da41b548a98f5
John P. Bloch <johnpbloch@gmail.com>
2011-09-05 11:33:24 -0400

    Add a 'create_user' function

--- lib/functions/users.php
+++ lib/functions/users.php
@@ -18,3 +18,27 @@ function get_user( $id_or_login_or_email ){
 	$user = $result->fetch_object( '\\' . __NAMESPACE__ . '\\User' );
 	return $user;
 }
+
+function create_user( $details ){
+	$defaults = array(
+		'Login' => false,
+		'Email' => false,
+		'DisplayName' => false,
+		'DateCreated' => false,
+		'Pass' => false,
+	);
+	$details = array_merge( $defaults, array_intersect_key( (array)$details, $defaults ) );
+	if( empty( $details[ 'Login' ] ) || empty( $details[ 'Email' ] ) || !is_email( $details[ 'Email' ] ) || empty( $details[ 'Pass' ] ) )
+		return false;
+	if( empty( $details[ 'DisplayName' ] ) )
+		$details[ 'DisplayName' ] = $details[ 'Login' ];
+	if( !preg_match( '|^\d{4}(-\d\d){2} \d\d(:\d\d){2}$|', (string)$details[ 'DateCreated' ] ) )
+		$details[ 'DateCreated' ] = date( 'Y-m-d H:i:s' );
+	$hasher = new \PasswordHash( 16, false );
+	$details[ 'Pass' ] = $hasher->HashPassword( $details[ 'Pass' ] );
+	$DB = db();
+	$existing_user_check = $DB->get_results( $DB->prepare( "SELECT `ID` FROM $DB->Users WHERE `Login` = %s OR `Email` = %s", $details[ 'Login' ], $details[ 'Email' ] ) );
+	if( empty( $existing_user_check ) )
+		return $DB->insert( 'Users', $details );
+	return false;
+}

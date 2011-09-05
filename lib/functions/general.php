10e5f03c9fd75309f8e92a0b3397303de46ca650
John P. Bloch <johnpbloch@gmail.com>
2011-09-05 10:54:51 -0400

    Formatting and adding is_email function.

--- lib/functions/general.php
+++ lib/functions/general.php
@@ -15,6 +15,10 @@ namespace MenuBuddy {
 		return $database;
 	}
 
+	function is_email( $maybe_email ){
+		return preg_match( '|^([-_a-z0-9]+(\.[-_a-z0-9]+)*)@([-a-z0-9]+(\.[-a-z0-9]+)*(\.[a-z])+)$|i', $maybe_email );
+	}
+
 }
 
 namespace {

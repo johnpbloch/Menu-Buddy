4a615746fc657c194b3ea8d79318a4885ea6a5b9
John P. Bloch <johnpbloch@gmail.com>
2011-10-30 18:30:46 -0400

    Add template initialization into the load sequence.

--- load.php
+++ load.php
@@ -5,7 +5,10 @@ namespace MenuBuddy;
 require( PATH . 'config.php' );
 require( FUNC . 'general.php' );
 require( FUNC . 'users.php' );
+require( FUNC . 'template.php' );
 
 initialize_database();
 
 Extensions::initialize();
+
+Template\startup_check();

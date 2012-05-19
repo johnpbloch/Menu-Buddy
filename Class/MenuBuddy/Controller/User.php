39fde5134dea684b4576455112e51e008f84e81b
John P. Bloch <johnpbloch@gmail.com>
2012-05-19 16:33:13 -0400

    User controller

---
+++ Class/MenuBuddy/Controller/User.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace MenuBuddy\Controller;
+
+class User extends \MenuBuddyBase
+{
+
+	protected function create_form()
+	{
+		
+	}
+
+	public function post( $action = '', $user = false )
+	{
+		$this->load_database();
+		switch( $action )
+		{
+			case 'create':
+				break;
+			case 'delete':
+				break;
+			case 'edit':
+				break;
+			default:
+				$this->show_404();
+				break;
+		}
+	}
+
+	public function get( $action = '', $user = false )
+	{
+		$this->load_database();
+		switch( $action )
+		{
+			case 'create':
+				break;
+			case 'delete':
+				break;
+			case 'edit':
+				break;
+			case 'list':
+				break;
+			case 'profile':
+				break;
+			default:
+				$this->show_404();
+				break;
+		}
+	}
+
+	public function run()
+	{
+		$this->show_404();
+	}
+
+}

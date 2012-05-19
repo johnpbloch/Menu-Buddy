389a3ee620da344e2a503a2a7249b858da5f162c
John P. Bloch <johnpbloch@gmail.com>
2012-05-19 16:04:12 -0400

    Users database table

--- Config/Migration.php
+++ Config/Migration.php
@@ -1,4 +1,14 @@
 <?php
 
 $config = array(
+	'users' => array(
+		'id' => array( 'type' => 'primary' ),
+		'username' => array( 'type' => 'string', 'length' => 60, 'unique' => true, 'null' => false ),
+		'email' => array( 'type' => 'string', 'length' => 100, 'unique' => true, 'null' => false ),
+		'pass' => array( 'type' => 'string', 'length' => 60, 'null' => false ),
+		'active' => array( 'type' => 'boolean', 'default' => false ),
+		'activation_key' => array( 'type' => 'string', 'length' => 60 ),
+		'account_created' => array( 'type' => 'datetime' ),
+		'last_login' => array( 'type' => 'datetime' ),
+	),
 );

0b15a1e06812ef1d3599e4d4d95eb61edbd26742
John P. Bloch <johnpbloch@gmail.com>
2012-06-02 17:09:07 -0400

    Better header.

--- View/MenuBuddy/Header.php
+++ View/MenuBuddy/Header.php
@@ -1,1 +1,18 @@
-<h1>This is the header.</h1>
+<div id="wrap">
+	<div class="container">
+		<header>
+			<div class="row">
+				<div class="five columns">
+					<div id="logoBox" class="valign row">
+						<div>
+							<img src="/Images/logo.png" />
+						</div>
+						<div>
+							<h2>MenuBuddy</h2>
+						</div>
+					</div>
+				</div>
+				<div class="seven columns"></div>
+			</div>
+		</header>
+		<div class="row" id="main">

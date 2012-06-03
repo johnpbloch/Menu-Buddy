a207a8395c0825d675b9cd53c34cf54ff5b7046f
John P. Bloch <johnpbloch@gmail.com>
2012-06-03 16:54:21 -0400

    Add an actions menu to the header.

--- View/MenuBuddy/Header.php
+++ View/MenuBuddy/Header.php
@@ -12,7 +12,21 @@
 						</div>
 					</div>
 				</div>
-				<div class="seven columns"></div>
+				<div class="seven columns">
+					<ul id="actionsMenu" class="subnav right">
+						<li><a href="/login">Log in</a> &nbsp;|</li>
+						<li><a href="/register">Register</a> &nbsp;|</li>
+						<li>
+							<div class="field">
+								<form action="/search" method="get">
+									<p class="text search oval">
+										<input placeholder="Search..." type="search">
+									</p>
+								</form>
+							</div>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</header>
 		<div class="row" id="main">
